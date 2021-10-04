7cb74a37cba2aa101651492e6db1e0dfa68b2cc1
Fernando Blanco <renefernando2001@gmail.com>
2021-10-03 18:28:33 -0600

    Added editar alumnos
    Alumnos now can be updated

---
+++ Db/editarAlumno.php
@@ -0,0 +1,99 @@
+<?php   
+    include_once 'sesiones.php';
+    require("conexion.php");
+
+    $edad="";$nombre="";$locacion="";$id="";$error=""; $nacimiento="";$padre="";$padreTel="";$madre="";$madreTel="";$contacto="";
+    $nuevalocacion="";
+
+    if(empty($_POST['edad'])) $error = "falta edad";
+    else $edad = $_POST['edad'];
+
+	if(empty($_POST['nacimiento'])) $error .= "falta nacimiento";
+	else $nacimiento = $_POST['nacimiento'];
+
+    if(empty($_POST['nombre'])) $error .= "falta nombre";
+    else $nombre = $_POST['nombre'];
+
+	if(empty($_POST['padre'])) $error .= "falta padre";
+	else $padre = $_POST['padre'];
+
+	if(empty($_POST['padreTel'])) $error .= "falta padreTel";
+	else $padreTel = $_POST['padreTel'];
+
+	if(empty($_POST['madre'])) $error .= "falta madre";
+	else $madre = $_POST['madre'];
+
+	if(empty($_POST['madreTel'])) $error .= "falta madreTel";
+	else $madreTel = $_POST['madreTel'];
+
+	if(empty($_POST['contacto'])) $error .= "falta contacto";
+	else $contacto = $_POST['contacto'];
+
+    if(empty($_POST['id'])) $error .= "falta id";
+    else $id = $_POST['id'];
+
+    if( $_FILES['img'] ) $locacion="hay imagen";
+    else $error .= "falta img";
+
+    //con imagen
+    if( $edad!=="" && $nombre!=="" && $nacimiento!=="" && $id!=="" && $padre!=="" && $padreTel!=="" && $madre!=="" && $madreTel!=="" && $contacto!==""){
+        
+        $conexion = conectar();
+        $AlumnoActual  = $conexion ->prepare ("SELECT Nombre, Edad, Fecha_Nacimiento,
+       	Nombre_Padre, Telefono_Padre, Nombre_Madre, Telefono_Madre, Contacto from Alumnos WHERE ID = :id ");
+        $AlumnoActual->bindParam(':id', $id, PDO::PARAM_STR);
+        $AlumnoActual->execute();
+        foreach($AlumnoActual as list($anombre, $aedad, $anacimiento, $apadre, $apadreTel, $amadre, $amadreTel, $acontacto )){
+
+                $imagenes = $conexion ->prepare("SELECT imagen from Alumnos WHERE ID = :id ");
+                $imagenes->bindParam(':id', $id, PDO::PARAM_STR);
+                $imagenes->execute();
+
+                foreach($imagenes as list($imagen)){
+	                if($imagen !="" && $imagen!=NULL && $imagen!="null"){
+						if($_FILES['img']['name']){
+							$locacion = '../assets/img/EduSoccer/Alumnos/' . $imagen;
+							$success = unlink($locacion);
+							move_uploaded_file($_FILES['img']['tmp_name'], $locacion);
+						}
+		                else $success = true;
+						if($success)http_response_code(200);
+						else http_response_code(500);
+	                }
+					else{
+						$imagen = $_FILES['img']['name'];
+						$locacion = '../assets/img/EduSoccer/Alumnos/' . $imagen;
+						move_uploaded_file($_FILES['img']['tmp_name'], $locacion);
+						$editarAlumno = $conexion->prepare(" UPDATE Alumnos SET Edad=:Edad, Nombre=:Nombre, Fecha_Nacimiento=:Fecha_Nacimiento, 
+						Nombre_Padre=:Nombre_Padre, Telefono_Padre=:Telefono_Padre, Nombre_Madre=:Nombre_Madre, Telefono_Madre=:Telefono_Madre,
+                    	Contacto=:Contacto, Imagen=:Imagen WHERE ID=:ID ");
+						$editarAlumno->bindParam(':Edad', $edad, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Nombre', $nombre, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Fecha_Nacimiento', $nacimiento, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Nombre_Padre', $padre, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Telefono_Padre', $padreTel, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Nombre_Madre', $madre, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Telefono_Madre', $madreTel, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Contacto', $contacto, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':Imagen', $imagen, PDO::PARAM_STR);
+						$editarAlumno->bindParam(':ID', $id, PDO::PARAM_STR);
+						$editarAlumno->execute();
+
+						if($editarAlumno->rowCount() >= 1){
+							echo $editarAlumno->execute();
+							http_response_code(200);
+						}else{
+							echo $editarAlumno->execute();
+							echo $imagen;
+							http_response_code(500);
+						}
+					}
+                }
+            }
+
+    }
+    else{
+        http_response_code(404);
+        echo $error;
+    }
+?>
