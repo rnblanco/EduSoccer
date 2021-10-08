e0eae741c0fc07948abe1cf081abd6cdf0c51349
Fernando Blanco <renefernando2001@gmail.com>
2021-10-08 13:12:08 -0600

    No index redirect

--- Admin/Views/index.view.php
+++ Admin/Views/index.view.php
@@ -1,51 +1,129 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 	<head>
+
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" type="image/x-icon" href="Images/DefaultImages/icon.ico"/>
-		<title>Index</title>
+		<link rel="icon" type="image/x-icon" href="Images/DefaultImages/icon.ico" />
+
+		<title>Alumnos</title>
+
+		<!-- Custom fonts for this template -->
 		<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 		<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
 		<link href="css/sb-admin-2.css" rel="stylesheet">
-		<link href="css/form.css" rel="stylesheet">
+
+		<!-- Custom styles for this page -->
+		<link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 	</head>
 
 	<body id="page-top">
+
 		<!-- Page Wrapper -->
 		<div id="wrapper">
+
 			<!-- Sidebar -->
 			<?php
 				require 'menu.php';
 				Menu($_SESSION['tipo']);
 			?>
+			<!-- End of Sidebar -->
+
+			<!-- Content Wrapper -->
 			<div id="content-wrapper" class="d-flex flex-column">
+
+				<!-- Main Content -->
 				<div id="content">
+
+					<!-- Topbar -->
 					<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-						<form class="form-inline justify-content-center">
-							<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3 mt-3">
+
+						<!-- Sidebar Toggle (Topbar) -->
+						<form class="form-inline">
+							<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
 								<i class="fa fa-bars"></i>
 							</button>
 						</form>
+
+						<!-- Topbar Navbar -->
 						<ul class="navbar-nav ml-auto">
+
 							<div class="topbar-divider d-none d-sm-block"></div>
+
+							<!-- Nav Item - User Information -->
 							<?php
 								User();
 							?>
+
 						</ul>
+
 					</nav>
+					<!-- End of Topbar -->
+
 					<!-- Begin Page Content -->
-					<div class="container-fluid"></div>
+					<div class="container-fluid">
+
+						<!-- DataTales Example -->
+						<div class="card shadow mb-4">
+							<div class="card-header py-3">
+								<h4 class="m-0 font-weight-bold text-primary">Todos los Alumnos</h4>
+								<div class="d-flex justify-content-end">
+									<a href="agregar-alumno.php" class="btn btn-success btn-icon-split">
+                <span class="icon text-white-50">
+                  <i class="fas fa-plus"></i>
+                </span>
+										<span class="text">Agregar alumno</span>
+									</a>
+								</div>
+
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+										<thead>
+											<tr>
+												<th>ID</th>
+												<th>Categoría</th>
+												<th>Nombre</th>
+												<th>Edad</th>
+												<th>Fecha de nacimiento</th>
+												<th>Fecha de ingreso</th>
+												<th>Fecha de matrícula</th>
+												<th>Nombre padre</th>
+												<th>Teléfono</th>
+												<th>Nombre madre</th>
+												<th>Teléfono</th>
+												<th>Contacto</th>
+												<th>Imagen</th>
+												<th>Acciones</th>
+											</tr>
+										</thead>
+										<tbody>
+											<?php Alumnos()?>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<!-- /.container-fluid -->
+
 				</div>
 				<!-- Footer -->
 				<?php require 'footer.php';?>
 			</div>
+			<!-- End of Content Wrapper -->
+
 		</div>
+		<!-- End of Page Wrapper -->
 
 		<!-- Scroll to Top Button-->
 		<a class="scroll-to-top rounded" href="#page-top">
@@ -72,10 +150,14 @@
 		</div>
 
 		<script src="vendor/jquery/jquery.js"></script>
-		<script src="vendor/jquery-easing/jquery.easing.js"></script>
+		<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+		<script src="vendor/datatables/jquery.dataTables.min.js"></script>
+		<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 		<script src="vendor/bootstrap/js/bootstrap.bundle.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 		<script src="js/sb-admin-2.js"></script>
+		<script src="js/demo/datatables-alumnos.js"></script>
+		<script src="js/alumnos.js"></script>
 	</body>
+
 </html>
--- Admin/index.php
+++ Admin/index.php
@@ -1,6 +1,69 @@
 <?php
-	session_start();
-    $usuario = $_SESSION['usuario'] || null;
-    if($usuario == null || $usuario = '') header("Location: ../Db/logOut.php");
-    else require_once 'Views/index.view.php';
+	require("../Db/conexion.php");
+	require_once '../Db/sesiones.php';
+
+	$usuario = $_SESSION['usuario'];
+	if($usuario == null || $usuario = '') header("Location: ../Db/logOut.php");
+	else require_once 'Views/index.view.php';
+
+	/*
+	Orden de tabla publicaciones:
+	<th>ID</th>
+	<th>Categoría</th>
+	<th>Nombre</th>
+	<th>Edad</th>
+	<th>Fecha de nacimiento</th>
+	<th>Fecha de ingreso</th>
+	<th>Fecha de matrícula</th>
+	<th>Imagen</th>
+	<th>Nombre padre</th>
+	<th>Teléfono</th>
+	<th>Nombre madre</th>
+	<th>Teléfono</th>
+	<th>Contacto</th>
+	*/
+
+	function Alumnos(){
+		$conexion = conectar();
+		$buscarAlumnos = $conexion->prepare("SELECT * FROM alumnos ORDER BY ID");
+		$buscarAlumnos->execute();
+		$Alumnos = $buscarAlumnos->fetchAll();
+
+		foreach($Alumnos as list($id, $nombre, $edad, $nacimiento, $ingreso, $matricula, $imagen, $padre, $padreTel, $madre, $madreTel, $contacto, $categoria)){
+			$imagen==""?$imagen="default_user.png":$imagen;
+			$padre = $padre!='null'? $padre : '-';
+			$padreTel = $padreTel!='null'? $padreTel : '-';
+			$madre = $madre!='null'? $madre : '-';
+			$madreTel = $madreTel!='null'? $madreTel : '-';
+
+			$buscarCategorias = $conexion->prepare("SELECT * FROM categorias WHERE ID=:ID");
+			$buscarCategorias->bindParam(':ID', $categoria, PDO::PARAM_STR);
+			$buscarCategorias->execute();
+			$Categorias = $buscarCategorias->fetchAll();
+			foreach($Categorias as list($id2, $titulo2, $subtitulo2, $contenido2, $profesor2, $imagen2)){ $categoria = $titulo2; }
+
+			echo" 
+                <tr>
+                    <td>$id</td>
+                    <td>$categoria</td>
+                    <td>$nombre</td>
+                    <td>$edad</td>
+                    <td>$nacimiento</td>
+                    <td>$ingreso</td>
+                    <td>$matricula</td>
+                    <td>$padre</td>
+                    <td>$padreTel</td>
+                    <td>$madre</td>
+                    <td>$madreTel</td>
+                    <td>$contacto</td>
+                    <td><img class='card-img-top' src='../assets/img/EduSoccer/Alumnos/$imagen' alt='Card image cap' style='height:100px!important;width:150px!important;'></td>            
+                    <td>
+                        <a href='editar-alumno.php?id=$id' class='btn btn-warning btn-icon-split btn-block'><span class='icon text-white-50'><i class='fas fa-edit'></i></span><span class='text'>Editar</span></a><div class='my-2'></div>
+                        <a style='color:white' class='btn btn-danger btn-icon-split btn-block' onclick='del($id)'><span class='icon text-white-50'><i class='fas fa-trash'></i></span><span class='text'>Borrar</span></a>
+                    </td>
+                </tr>
+            ";
+		}
+	}
+
 ?>
