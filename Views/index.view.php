3caf6c646180921e5cc17379a363e11cc874b9ba
Fernando Blanco <renefernando2001@gmail.com>
2021-10-09 13:30:04 -0600

    Menu and categorias bugfix

--- Views/index.view.php
+++ Views/index.view.php
@@ -163,8 +163,9 @@
 		<!-- Bootstrap core JS-->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 		<!-- Core theme JS-->
 		<script src="js/scripts.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
 	</body>
 </html>
