591264d0d99b505c2f35d28896ac5b815607ad33
Fernando Blanco <renefernando2001@gmail.com>
2021-10-09 10:49:49 -0600

    Session duration set to 1 hour

--- Db/sesiones.php
+++ Db/sesiones.php
@@ -1,3 +1,25 @@
 <?php 
     session_start();
+	// Máxima duración de sesión activa en hora
+	define( 'MAX_SESSION_TIEMPO', 3600 * 1 );
+
+	// Controla cuando se ha creado y cuando tiempo ha recorrido
+	if ( isset( $_SESSION[ 'ULTIMA_ACTIVIDAD' ] ) &&
+		( time() - $_SESSION[ 'ULTIMA_ACTIVIDAD' ] > MAX_SESSION_TIEMPO ) ) {
+		// Si ha pasado el tiempo sobre el limite destruye la session
+		destruir_session();
+	}
+
+	$_SESSION[ 'ULTIMA_ACTIVIDAD' ] = time();
+
+	// Función para destruir y resetear los parámetros de sesión
+	function destruir_session(){
+		$_SESSION = [];
+		if(ini_get('session.use_cookies')){
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - MAX_SESSION_TIEMPO, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+		}
+		@session_destroy();
+	}
+
 ?>
